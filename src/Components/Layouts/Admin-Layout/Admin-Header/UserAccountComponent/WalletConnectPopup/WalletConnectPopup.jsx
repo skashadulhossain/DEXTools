81278b68b410fc217acc71eee90cbadfc755bc4c
junayed <junayedmollick536@gmail.com>
2024-09-11 22:41:55 +0530

    changes in pages

--- src/Components/Layouts/Admin-Layout/Admin-Header/UserAccountComponent/WalletConnectPopup/WalletConnectPopup.jsx
+++ src/Components/Layouts/Admin-Layout/Admin-Header/UserAccountComponent/WalletConnectPopup/WalletConnectPopup.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './WalletConnectPopup.css';
+import iconimg1 from './Image/Icon img 1.webp';
+import iconimg2 from './Image/Icon img 2.webp';
+import iconimg3 from './Image/Icon img 3.webp';
+import iconimg4 from './Image/Icon img 4.webp';
+import iconimg5 from './Image/Icon img 5.webp';
+import iconimg6 from './Image/Icon img 6.png';
 
 const wallets = [
-  { id: 1, name: 'WalletConnect', icon: 'wallet-connect-icon.png', qrCode: true },
-  { id: 2, name: 'OKX Wallet', icon: 'okx-wallet-icon.png' },
-  { id: 3, name: 'MetaMask', icon: 'metamask-icon.png' },
-  { id: 4, name: 'Binance Web3 Wallet', icon: 'binance-web3-icon.png' },
-  { id: 5, name: 'Coinbase', icon: 'coinbase-icon.png' },
-  { id: 6, name: 'All Wallets', icon: 'all-wallets-icon.png', count: '430+' },
+  { id: 1, name: 'WalletConnect', icon: iconimg1, qrCode: true },
+  { id: 2, name: 'OKX Wallet', icon: iconimg2 },
+  { id: 3, name: 'MetaMask', icon: iconimg3 },
+  { id: 4, name: 'Binance Web3 Wallet', icon: iconimg4 },
+  { id: 5, name: 'Coinbase', icon: iconimg5 },
+  { id: 6, name: 'All Wallets', icon: iconimg6, count: '430+' },
 ];
 
 const WalletConnectPopup = ({ onClose }) => {
--- src/Pages/UserAccount/UserAccount.js
+++ src/Pages/UserAccount/UserAccount.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserAccountComponent from '../../Components/Layouts/Admin-Layout/Admin-Header/UserAccountComponent/UserAccountComponent'
 import Sidebar from '../../Components/Layouts/Admin-Layout/Admin-Sidebar/SideBar';
+import Footer from '../../Components/Layouts/Admin-Layout/Admin-Footer/AdminFooter';
 
 const UserAccount = () => {
   return (
@@ -8,6 +9,7 @@ const UserAccount = () => {
     <div>
       <UserAccountComponent/>
     </div>
+    <Footer />
     </Sidebar>
   )
 }
